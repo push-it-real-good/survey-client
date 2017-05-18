9577b48473121d0008262ee50834e2bf5c0bd744
octater <terry.gillespie@libertymutual.com>
2017-05-18 13:10:53 -0400

    Survey client fix
    updated assets/scripts/config.js file to include // in url for
    Heroku.
    
    Paired with @conorjennings for implementation

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
     // production: 'https://ga-wdi-boston.herokuapp.com'
-    production: 'https:floating-savannah-15725.herokuapp.com'
+    production: 'https://floating-savannah-15725.herokuapp.com'
   }
 }
 
