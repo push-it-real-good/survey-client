322a0e419dc9ed737703f85b38eb18c1339c0012
MattKwonDo <mgduvall@gmail.com>
2017-05-18 14:14:15 -0400

    create response works, but not well
    pre-fill / auto load of survey_id is needed.

--- assets/scripts/response/events.js
+++ assets/scripts/response/events.js
@@ -4,13 +4,11 @@ const getFormFields = require(`../../../lib/get-form-fields`)
 const api = require('./api')
 const ui = require('./ui')
 // const authUi = require('../auth/ui')
-const store = require('../store')
+// const store = require('../store')
 
 const onCreateResponse = function (event) {
   event.preventDefault()
   console.log('createresponse')
-  debugger;
-  return
 
   const data = getFormFields(this)
   console.log(data)
@@ -19,11 +17,11 @@ const onCreateResponse = function (event) {
     ui.createResponseSuccess(data)
   })
   .catch(ui.createResponseFailure)
-  console.log('++++ onCreateResponse(), token = ', store.user.token)
+  // console.log('++++ onCreateResponse(), token = ', store.user.token)
 }
 
 const addHandlers = () => {
-  $('#create_response').on('submit', onCreateResponse)
+  $('#create-response').on('submit', onCreateResponse)
 }
 
 module.exports = {
