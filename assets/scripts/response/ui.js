27d0ab9a144b7c3594d529e6ae3ce7bb4ed98312
MattKwonDo <mgduvall@gmail.com>
2017-05-18 20:41:05 -0400

    remove erroneous code, logs from scripts/response

--- assets/scripts/response/ui.js
+++ assets/scripts/response/ui.js
@@ -14,37 +14,16 @@ const createResponseFailure = () => {
 }
 
 const getResponsesSuccess = (data) => {
-  console.log('data after api call', data)
+  // console.log('data after api call', data)
   const showResponsesHtml = showResponsesTemplate({ responses: data.responses })
   store.responses = data.responses
-  console.log('>>>>> store.responses = ', store.responses)
+  // console.log('>>>>> store.responses = ', store.responses)
   $('#responses-content').html(showResponsesHtml)
 }
 
-const getResponsesFailure = (error) => {
-  console.error(error)
+const getResponsesFailure = () => {
+  // console.error(error)
 }
-
-// const getResponsesSuccess = (data) => {
-//   // store.user = data.user
-//   console.log(data)
-//   console.log(data.responses[0])
-//   console.log('text:', data.responses[0].text)
-//   console.log('title:', data.responses[0].title)
-//   console.log('response_id:', data.responses[0].response_id)
-//   console.log('question_id:', data.responses[0].question_id)
-//   console.log('respondent_id:', data.responses[0].respondent_id)
-//   const showResponsesHtml = showResponsesTemplate({ responses: data.responses })
-//   // debugger;
-//   $('.content').html(showResponsesHtml) // .order
-//   // $('.errors-create-item').empty()
-//   // $('#create-item').show()
-//   // $('#spacer1').text('List')
-// }
-//
-// const getResponsesFailure = (error) => {
-//   console.error(error)
-// }
 
 module.exports = {
   createResponseSuccess,
