0c16469784e02014e3c28fdbdce15cac5ed1e2eb
kqngo <khan.ngo@gmail.com>
2017-06-02 00:32:25 -0400

    Add dashboard based on data type
    Depending on survey type and response data type, the survey results will be different. Dashboard will report raw response data, capture aggregate of same response - converted to lowercase and compared.
    
    Each metric has the response, the # of occurrence of response, as well as the Percentage of occurences.

--- assets/scripts/response/ui.js
+++ assets/scripts/response/ui.js
@@ -30,6 +30,41 @@ const getResponsesSuccess = (data) => {
 
     $('#responses-content').show()
     $('#responses-content').html(showResponsesHtml)
+// build my array
+    const arrTextResponse = []
+    for (let i = 0; i < data.responses.length; i++) {
+      if (data.responses[i].text) {
+        arrTextResponse.push(data.responses[i].text)
+        // console.log(arrTextResponse)
+      }
+    }
+// arrTextResponse.sort()
+// arrTextResponse.toLowerCast()
+
+    // convert array to lowercase
+    for (let i = 0; i < arrTextResponse.length; i++) {
+    arrTextResponse[i] = arrTextResponse[i].toLowerCase()
+    }
+
+// Create Object with values of array and number of repeats
+
+    const result = {}
+    for (let i = 0; i < arrTextResponse.length; i++) {
+      if (!result[arrTextResponse[i]])
+        result[arrTextResponse[i]] = 0
+      ++result[arrTextResponse[i]]
+        }
+
+// print out contents of Hash
+    const total = Number(data.responses.length)
+
+    for (const key in result) {
+      const textResponseNum = Number(result[key])
+      const ratingAnswers4percent = ((textResponseNum / total) * 100)
+      const ratingAnswers4Percent = parseFloat(ratingAnswers4percent).toFixed(2)
+      $(`.textResponseInfo`).append('<tr>' + '<th>' + key + '</th> ' + '<th>' + result[key] + '</th>' + '<th>' + ratingAnswers4Percent + '%' + '</th>' + '</tr>')
+    }
+
     // console.log('object_Length: ', data.responses.length)
     let yesAnswers = 0
     let noAnswers = 0
@@ -41,7 +76,7 @@ const getResponsesSuccess = (data) => {
     let totalRatingCount = 0
 
     for (let i = 0; i < data.responses.length; i++) {
-      // Boolean Counts
+      // Boolean Metric Logic
       if (data.responses[i].boolean === true) {
         yesAnswers++
         const total = Number(data.responses.length)
@@ -49,8 +84,12 @@ const getResponsesSuccess = (data) => {
         const yesAnswerspercent = ((yesAnswersResponseNum / total) * 100)
         const yesAnswersPercent = parseFloat(yesAnswerspercent).toFixed(2)
 
-        $(`.count-true`).text(`${yesAnswers} respondent(s) selected "YES"`)
-        $(`.count-true-percent`).text(`${yesAnswersPercent}% of respondents selected "YES"`)
+        // $(`.count-true`).text(`${yesAnswers} respondent(s) selected "YES"`)
+        // $(`.count-true-percent`).text(`${yesAnswersPercent}% of respondents selected "YES"`)
+          // if (i === data.responses.length -1) {
+            $(`.booleanResponseData1`).html('<tr>' + '<th>' + 'YES' + '</th> ' + '<th>' + `${yesAnswers}` + '</th>' + '<th>' + `${yesAnswersPercent}` + '%' + '</th>' + '</tr>')
+          // }
+
       } else if (data.responses[i].boolean === false) {
         noAnswers++
         const total = Number(data.responses.length)
@@ -58,12 +97,14 @@ const getResponsesSuccess = (data) => {
         const noAnswerspercent = ((noAnswersResponseNum / total) * 100)
         const noAnswersPercent = parseFloat(noAnswerspercent).toFixed(2)
 
-        $(`.count-false`).text(`${noAnswers} respondent(s) selected "NO"`)
-        $(`.count-false-percent`).text(`${noAnswersPercent}% of respondents selected "NO"`)
+        // $(`.count-false`).text(`${noAnswers} respondent(s) selected "NO"`)
+        // $(`.count-false-percent`).text(`${noAnswersPercent}% of respondents selected "NO"`)
+        // if (i === data.responses.length -1) {
+          $(`.booleanResponseData2`).html('<tr>' + '<th>' + 'NO' + '</th> ' + '<th>' + `${noAnswers}` + '</th>' + '<th>' + `${noAnswersPercent}` + '%' + '</th>' + '</tr>')
+        // }
       }
 
-      // Rating Counts
-
+      // Ratings Meric Logic
       if (data.responses[i].rating) {
         const total = Number(data.responses.length)
         if (data.responses[i].rating === 1) {
@@ -73,18 +114,15 @@ const getResponsesSuccess = (data) => {
           const ratingAnswers1percent = ((ratingAnswers1ResponseNum / total) * 100)
           const ratingAnswers1Percent = parseFloat(ratingAnswers1percent).toFixed(2)
 
-          $(`.ratingAnswers1`).text(`${ratingAnswers1} respondent(s) selected "1"`)
-          $(`.ratingAnswers1percent`).text(`${ratingAnswers1Percent}% of respondents selected "1"`)
+          $(`.numberResponseData1`).html('<tr>' + '<th>' + 'One' + '</th> ' + '<th>' + `${ratingAnswers1}` + '</th>' + '<th>' + `${ratingAnswers1Percent}` + '%' + '</th>' + '</tr>')
         } else if (data.responses[i].rating === 2) {
           ratingAnswers2++
           const ratingAnswers2ResponseNum = Number(ratingAnswers2)
           totalRatingCount = (totalRatingCount + 2)
-
           const ratingAnswers2percent = ((ratingAnswers2ResponseNum / total) * 100)
           const ratingAnswers2Percent = parseFloat(ratingAnswers2percent).toFixed(2)
 
-          $(`.ratingAnswers2`).text(`${ratingAnswers2} respondent(s) selected "2"`)
-          $(`.ratingAnswers2percent`).text(`${ratingAnswers2Percent}% of rrespondents selected "2"`)
+          $(`.numberResponseData2`).html('<tr>' + '<th>' + 'Two' + '</th> ' + '<th>' + `${ratingAnswers2}` + '</th>' + '<th>' + `${ratingAnswers2Percent}` + '%' + '</th>' + '</tr>')
         } else if (data.responses[i].rating === 3) {
           ratingAnswers3++
           const ratingAnswers3ResponseNum = Number(ratingAnswers3)
@@ -92,8 +130,7 @@ const getResponsesSuccess = (data) => {
           const ratingAnswers3percent = ((ratingAnswers3ResponseNum / total) * 100)
           const ratingAnswers3Percent = parseFloat(ratingAnswers3percent).toFixed(2)
 
-          $(`.ratingAnswers3`).text(`${ratingAnswers3} respondent(s) selected "3"`)
-          $(`.ratingAnswers3percent`).text(`${ratingAnswers3Percent}% of respondents selected "3"`)
+          $(`.numberResponseData3`).html('<tr>' + '<th>' + 'Three' + '</th> ' + '<th>' + `${ratingAnswers3}` + '</th>' + '<th>' + `${ratingAnswers3Percent}` + '%' + '</th>' + '</tr>')
         } else if (data.responses[i].rating === 4) {
           ratingAnswers4++
           const ratingAnswers4ResponseNum = Number(ratingAnswers4)
@@ -101,8 +138,7 @@ const getResponsesSuccess = (data) => {
           const ratingAnswers4percent = ((ratingAnswers4ResponseNum / total) * 100)
           const ratingAnswers4Percent = parseFloat(ratingAnswers4percent).toFixed(2)
 
-          $(`.ratingAnswers4`).text(`${ratingAnswers4} respondent(s) selected "4"`)
-          $(`.ratingAnswers4percent`).text(`${ratingAnswers4Percent}% of respondents selected "4"`)
+          $(`.numberResponseData4`).html('<tr>' + '<th>' + 'Four' + '</th> ' + '<th>' + `${ratingAnswers4}` + '</th>' + '<th>' + `${ratingAnswers4Percent}` + '%' + '</th>' + '</tr>')
         } else if (data.responses[i].rating === 5) {
           ratingAnswers5++
           const ratingAnswers5ResponseNum = Number(ratingAnswers5)
@@ -110,24 +146,17 @@ const getResponsesSuccess = (data) => {
           const ratingAnswers5percent = ((ratingAnswers5ResponseNum / total) * 100)
           const ratingAnswers5Percent = parseFloat(ratingAnswers5percent).toFixed(2)
 
-          $(`.ratingAnswers5`).text(`${ratingAnswers5} respondent(s) selected "5"`)
-          $(`.ratingAnswers5percent`).text(`${ratingAnswers5Percent}% of respondents selected "5"`)
+          $(`.numberResponseData5`).html('<tr>' + '<th>' + 'Five' + '</th> ' + '<th>' + `${ratingAnswers5}` + '</th>' + '<th>' + `${ratingAnswers5Percent}` + '%' + '</th>' + '</tr>')
         }
         const ratingaverage = (totalRatingCount / total)
         const ratingAverage = parseFloat(ratingaverage).toFixed(2)
 
         $(`.ratingAverage`).text(`Average Rating for this Survey Question: ${ratingAverage} `)
       }
-      // if (data.responses[i].text) {
-      //     cont out = []
-      //     let counts={}
-      //   const item = arr[i]
-      //   counts[item] = counts[item] >= 1 ? counts[item] + 1 : 1
-      //   if (counts[item] === 2) {
-      //     out.push(item)
-      //   }
-      //   $(`.textDuplicate`).text(`Duplicate Responses: ${out}`)
-      // }
+
+      if (data.responses[i].rating) {
+        $('#text-response-data').show()
+      }
     }
   } else {
     store.responses = data.responses
